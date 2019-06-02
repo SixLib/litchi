aaa450da7095d7d2a49370f378dd2968bd2ac340
sixlib <614044401@qq.com>
2019-06-02 15:18:34 +0800

    submit

--- examples/server.js
+++ examples/server.js
@@ -1,4 +1,4 @@
-const Litchi = require('./lib/licthi');
+const Litchi = require('../lib/licthi');
 const fs = require('fs');
 const path = require('path');
 const litchi = new Litchi();
@@ -9,9 +9,15 @@ litchi.options = {
   ),
   cert: fs.readFileSync(
     path.resolve(__dirname, 'certificate/localhost-cert.pem')
-  ),
-  allowHTTP1:true
+  )
 };
+// cors
+litchi.use((ctx, next) => {
+  ctx.res.setHeader('Access-Control-Allow-Origin', '*');
+  ctx.res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS'); //设置方法
+  next();
+});
 litchi.routers = [{
   method: 'POST',
   path: '/',
